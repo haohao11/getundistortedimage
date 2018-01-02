76135a92b4c70fc36a595fa3938016cea3b5b712
Hao Cheng <chenghao605@gmail.com>
2018-01-02 15:27:08 +0100

    change the extractor file

--- extractor.py
+++ extractor.py
@@ -5,13 +5,8 @@ Requirements
 ----
 You require OpenCV 3.2 to be installed.
 
-Run
-----
-Open the main.py and edit the path to the video. Then run:
-$ python main.py
-
-Which will produce a folder called data with the images. There will be 2000+ images for example.mp4.
 '''
+
 import cv2
 import numpy as np
 import os
@@ -24,6 +19,7 @@ currentFrame = 0
 slot = 1
 vidcap.set(cv2.CAP_PROP_POS_MSEC,2000)
 
+# This is the camera parameters for HA datasets
 # camera matrix
 K = np.array([[  1410.7395877898227,     0.  ,  1317.4509679240998],
               [    0.  ,   1401.2597462184024,   680.3912106607918],
@@ -47,28 +43,20 @@ if not os.path.exists(data_dir):
 
 success = True
 while success:
-       # added this line 
+    # added this line 
     success,image = vidcap.read()
     print ('Read a new frame: ', success)
-    if slot < 10:
-        name = './HA_2/0000' + str(slot) + '.jpg'
-    elif slot < 100 and slot >= 10:
-        name = './HA_2/000' + str(slot) + '.jpg'
-    elif slot < 1000 and slot >= 100:
-        name = './HA_2/00' + str(slot) + '.jpg'
-    elif slot < 10000 and slot >= 1000:
-        name = './HA_2/0' + str(slot) + '.jpg'
     #cv2.imwrite(name, image) # save frame as JPEG file
     currentFrame = currentFrame + 1
     if currentFrame%15 == 0:
         #cv2.imwrite(name, image)
         slot = slot + 1
         if slot >= 0:
-            #img_undistorted = cv2.fisheye.undistortImage(image, K, D=D, Knew=Knew)            
+            img_undistorted = cv2.fisheye.undistortImage(image, K, D=D, Knew=Knew)            
             #cv2.imwrite(name, img_undistorted)
             #cv2.imwrite(name, image)
-            cv2.imwrite(os.path.join(data_dir, "%05d.jpg" % slot), image)             
-    if currentFrame >= 150000:
+            cv2.imwrite(os.path.join(data_dir, "%05d.jpg" % slot), img_undistorted)             
+    if currentFrame >= 99999:
         break     
     
 # When everything done, release the capture
